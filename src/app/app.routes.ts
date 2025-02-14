0880d0117595cec74a0a09ad8440b221c3d73e4f
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-02-14 19:28:49 +0900

    page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: 'page3', component: Page3Component }
+];
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page1',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './page1.component.html',
-  styleUrl: './page1.component.scss'
+  styleUrls: ['./page1.component.scss'],
 })
-export class Page1Component {
-
-}
+export class Page1Component {}
